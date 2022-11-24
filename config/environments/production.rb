37832e9ab9d814d83b7e9ccc416abcc17fa2aac3
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2022-11-24 12:01:26 +0000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.hosts << "rails-production-0b87.up.railway.app"
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
